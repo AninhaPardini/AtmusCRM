df30c0a2c3cf21c4fdedd228a12ea5be4bac83f8
AninhaPardini <anapardinisimoni@gmail.com>
2025-01-07 00:29:02 -0300

    add: seção de personas

--- src/app/page/home/home.component.ts
+++ src/app/page/home/home.component.ts
@@ -1,7 +1,8 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
 import { IntegrationsComponent } from '../../components/integrations/integrations.component';
 import { FeedbacksComponent } from "../../components/feedbacks/feedbacks.component";
 import { SevendaystestComponent } from "../../components/sevendaystest/sevendaystest.component";
+import throttle from 'lodash/throttle';
 
 @Component({
   selector: 'atm-home',
@@ -9,17 +10,46 @@ import { SevendaystestComponent } from "../../components/sevendaystest/sevendays
   styleUrls: ['./home.component.css'],
   imports: [IntegrationsComponent, FeedbacksComponent, SevendaystestComponent]
 })
-export class HomeComponent implements OnInit {
-  ngOnInit() {
 
+export class HomeComponent implements OnInit, OnDestroy {
+  private render!: () => void;
+
+  ngOnInit() {
     if (typeof document !== 'undefined') {
-      document.addEventListener('DOMContentLoaded', function() {
+      document.addEventListener('DOMContentLoaded', () => {
         const carouselTrack = document.querySelector('.carousel-track');
         if (carouselTrack) {
           const clone = carouselTrack.innerHTML;
           carouselTrack.innerHTML += clone;
         }
+
+        const players = Array.from(document.querySelectorAll('.js-play-on-screen'));
+        console.log('aaa', players);
+
+        this.render = throttle(() => players.forEach(playAnimation), 150);
+
+        function isOnScreen(el: any) {
+          let rect = el.getBoundingClientRect();
+          console.log('rect', rect);
+          return rect.top > 0 && rect.bottom < window.innerHeight;
+        }
+
+        function playAnimation(el: any) {
+          if (isOnScreen(el)) {
+            el.style.animationPlayState = 'running';
+          }
+        }
+
+        this.render();
+
+        window.addEventListener('scroll', this.render);
       });
+    }
+  }
+
+  ngOnDestroy() {
+    if (typeof document !== 'undefined') {
+      window.removeEventListener('scroll', this.render);
     }
   }
 }
