1ab8c69ae7a2cbdd0d131a5aeec47abebe93adaa
AninhaPardini <aninhapardini.work@gmail.com>
2025-01-20 01:21:22 -0300

    add: Adicionado footer

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,11 +2,12 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { HomeComponent } from "./page/home/home.component";
 import { NavbarComponent } from "./components/navbar/navbar.component";
+import { FooterComponent } from './components/footer/footer.component';
 
 
 @Component({
   selector: 'atm-root',
-  imports: [RouterOutlet, HomeComponent, NavbarComponent],
+  imports: [RouterOutlet, NavbarComponent, FooterComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
